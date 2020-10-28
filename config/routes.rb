08ddd555856cd40a283a5669ada0c30e577f058d
shota.kirameki <ozawa@ualberta.ca>
2020-10-28 05:57:44 +0000

    post

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  resources :users, only: [:index, :show, :new, :create]
+  get 'signup', to: 'users#new'
+  
+  resources :microposts, only: [:create, :destroy]
+end
